d898c92453206617a517f95d0955f223c1515c71
FurukawaKaito <k.furukawa.rkt@gmail.com>
2018-05-26 12:11:06 +0900

    5/26

---
+++ app/src/main/java/com/hassyadai/udonmap/constant/LocationsConstant.java
@@ -0,0 +1,7 @@
+package com.hassyadai.udonmap.constant;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class LocationsConstant {
+    public static final LatLng TAMURA = new LatLng(34.266506,133.935356);
+}
