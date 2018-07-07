b904a9d25fd64c408b4106d59497a83aab009bf7
BK16099 <k.furukawa.rkt@gamil.com>
2018-07-07 15:21:32 +0900

    とりあえずローカルで動く状態

---
+++ app/src/main/java/com/hassyadai/udonmap/data/Udonya.java
@@ -0,0 +1,10 @@
+package com.hassyadai.udonmap.data;
+
+public class Udonya {
+    public String name;
+    public double lat;
+    public double lng;
+    public boolean jimoto_flag;
+    public String recommend;
+    public String comment;
+}
